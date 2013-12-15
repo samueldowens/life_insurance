6ecd65b2a3664fe21feed6a999bb10518755f1a1
samueldowens <samueldowens@gmail.com>
2013-12-14 23:49:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LifeInsurance::Application.config.secret_key_base = '161c65c72d19794591f3b97e1edf932a19e60c6b23ccf5a183699aa351698dc14f23254e751a030b6ef9b62d9e103224d28510ed4660aabdef55626899900ed1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LifeInsurance::Application.config.session_store :cookie_store, key: '_life_insurance_session'
