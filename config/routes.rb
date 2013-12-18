6c2badf964e32d06443f7555d79d862641299352
samueldowens <samueldowens@gmail.com>
2013-12-18 13:02:37 -0500

    Cleaned up more code formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 LifeInsurance::Application.routes.draw do
  
   root 'insurance_queries#index'
-
-  # post 'insurance_queries#create'
-
+  
   post 'insurance_queries/create', to: 'insurance_queries#create', as: 'create'
   get  'insurance_queries/:id/', to: 'insurance_queries#show', as: 'show'
+
 end
